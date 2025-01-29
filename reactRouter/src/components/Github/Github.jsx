27876ecff74cdb9485dc8e322ce7f16aca77a480
Lalit sharma <Lsharma906850@gmail.com>
2025-01-29 11:52:26 +0530

    some changes

--- reactRouter/src/components/Github/Github.jsx
+++ reactRouter/src/components/Github/Github.jsx
@@ -1,29 +1,28 @@
 import React, { useEffect, useState } from 'react'
-import { data } from 'react-router-dom'
 import { useLoaderData } from 'react-router-dom'
 
 function Github() {
     const data = useLoaderData()
-    // const[data, setData] = useState([])
+    // const [data, setData] = useState([])
     // useEffect(() => {
-    //     fetch('https://github.com/LalitSharma87084')
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         console.log(data);
-    //         setData(data)
-    //     })
-    // },[])
-
-
+    //  fetch('https://api.github.com/users/hiteshchoudhary')
+    //  .then(response => response.json())
+    //  .then(data => {
+    //     console.log(data);
+    //     setData(data)
+    //  })
+    // }, [])
+    
   return (
-    <div className='text-center m-4 bg-gray-700 text-white 
-    p-4 text-4xl'>Github followers :{data.followers} </div>
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+    <img src={data.avatar_url} alt="Git picture" width={300} />
+    </div>
   )
 }
 
 export default Github
 
 export const githubInfoLoader = async () => {
-    const response = await fetch ('https://github.com/LalitSharma87084')
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
     return response.json()
 }
--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -1,30 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
 import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+import Layout from './Layout.jsx'
 
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
 
-// const router = createBrowserRouter([{
-//   path: '/',
-//   element : <Layout/>,
-//   children :[
-//     {
-//       path: "",
-//       element: <Home/>
-//     },
-//     {
-//       path : "about",
-//       element : <About/>
-//     }
-//   ]
-// }])
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
@@ -33,9 +38,9 @@ const router = createBrowserRouter(
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
       <Route 
-      loader= {githubInfoLoader}
-      path='github'
-       element={<Github />} 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />}
        />
     </Route>
   )
@@ -43,6 +48,6 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
